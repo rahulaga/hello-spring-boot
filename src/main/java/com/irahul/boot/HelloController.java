99e816dbda243b66bd5aa73c9c9932bb7be10b4c
rahul <rahulaga@gmail.com>
2017-02-21 12:54:04 -0800

    add hello controller

---
+++ src/main/java/com/irahul/boot/HelloController.java
@@ -0,0 +1,18 @@
+package com.irahul.boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Controller with GET /hello
+ * @author rahul
+ *
+ */
+@RestController
+public class HelloController {
+
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello(){
+		return "Hello World!";
+	}
+}
--- src/main/java/com/irahul/boot/HelloSpringBootApplication.java
+++ src/main/java/com/irahul/boot/HelloSpringBootApplication.java
@@ -2,7 +2,11 @@ package com.irahul.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Start application
+ * @author rahul
+ *
+ */
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
