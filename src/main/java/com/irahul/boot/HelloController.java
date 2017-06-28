c63e577b6811a5743b36734b74c9ee370ac90837
rahulaga <rahulaga@gmail.com>
2017-06-27 19:30:32 -0700

    adding post method

--- src/main/java/com/irahul/boot/HelloController.java
+++ src/main/java/com/irahul/boot/HelloController.java
@@ -15,4 +15,9 @@ public class HelloController {
 	public String hello(){
 		return "Hello World!";
 	}
+	
+	@RequestMapping(value="/hello", method=RequestMethod.POST)
+	public String helloPost(){
+		return "POST Hello World!";
+	}
 }
